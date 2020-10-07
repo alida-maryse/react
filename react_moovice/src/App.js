4c597bd071248624ddcec01830a960506ad827d9
alida-maryse <68588247+alida-maryse@users.noreply.github.com>
2020-10-07 16:54:17 +0200

    sauvegarde moovice

---
+++ react_moovice/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Discover from './components/Discover'
+import DiscoverBattle from './components/DiscoverBattle'
+import Popular from './components/Popular'
+import PopularBattle from './components/PopularBattle'
+import MyList from './components/MyList' 
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">This week</Link></li>
+              <li><Link to="/battle/">Battle</Link></li>
+              <li><Link to="/popular/">Popular</Link></li>
+              <li><Link to="/popular-battle/">Popular-battle</Link></li>
+              <li><Link to="/my-list/">My-list</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component= {Discover}>
+              <Discover />
+            </Route>
+            <Route exact path="/battle/" component= {DiscoverBattle}>
+              <DiscoverBattle />
+            </Route>
+            <Route exact path="/popular/" component= {Popular}>
+              <Popular />
+            </Route>
+            <Route exact path="/popular-battle/" component= {PopularBattle}>
+              <PopularBattle />
+            </Route>
+            <Route exact path="/my-list/" component= {MyList}>
+              <My-list />
+            </Route>       
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+export default App;
